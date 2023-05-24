faa18a58e97e066c8a72ad3f4ed8e006465d2b94
Rasel Mahmud <rasel.mahmud.dev@gmail.com>
2023-05-24 14:21:47 +0600

    handle navigation breadcrumbs

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,54 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React, {useEffect, useState} from 'react'
+import {Link, useLocation} from "react-router-dom";
 
 export default function Header() {
+
+    const location = useLocation()
+
+    const [breadcrumbsData, setBreadcrumbsData] = useState([])
+
+    useEffect(() => {
+        if (location.pathname === "/") {
+            setBreadcrumbsData([{label: "Home", path: "/"}])
+        } else if (location.pathname.includes("/checkout")) {
+            setBreadcrumbsData([
+                {label: "Home", path: "/"},
+                {label: "Checkout", path: location.pathname}
+            ])
+        } else if (location.pathname.includes("/join/login")) {
+            setBreadcrumbsData([
+                {label: "Home", path: "/"},
+                {label: "Login", path: location.pathname}
+            ])
+        } else if (location.pathname.includes("/join/new")) {
+            setBreadcrumbsData([
+                {label: "Home", path: "/"},
+                {label: "Register", path: location.pathname}
+            ])
+        } else if (location.pathname.includes("/add-product")) {
+            setBreadcrumbsData([
+                {label: "Home", path: "/"},
+                {label: "Add Product", path: location.pathname}
+            ])
+        }
+    }, [location]);
+
+
     return (
         <div className="bg-header h-12">
             <nav className="rounded-md ml-auto mr-auto container h-full">
                 <ol className="list-reset flex h-full items-center text-sm font-normal">
-                    <li>
-                        <Link to="/" className="text-white text-opacity-40 "
-                        >Home</Link>
-                    </li>
-                    <li>
-                        <span className="mx-2 text-white">/</span>
-                    </li>
-                    <li className="text-white dark:text-neutral-400">Checkout</li>
+                    {breadcrumbsData.map((item, index) => (
+                        <div className="flex items-center text-white text-opacity-60">
+                            {breadcrumbsData.length > 1 && index !== 0 && (
+                                <div className=" px-2">/</div>
+                            )}
+                            <li>
+                                <Link to={item.path} className="text-white text-opacity-60 "
+                                >{item.label}</Link>
+                            </li>
+                        </div>
+                    ))}
                 </ol>
             </nav>
         </div>
